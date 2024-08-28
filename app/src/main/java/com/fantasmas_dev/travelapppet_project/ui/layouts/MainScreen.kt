8e4db82cde4baf7e70466a3e3a97ebfcd3e31d19
FantasmasDe <fantasmas.dev@gmail.com>
2024-08-29 00:12:52 +0300

    Добавлены базовые элементы

--- app/src/main/java/com/fantasmas_dev/travelapppet_project/ui/layouts/MainScreen.kt
+++ app/src/main/java/com/fantasmas_dev/travelapppet_project/ui/layouts/MainScreen.kt
@@ -1,12 +1,57 @@
 package com.fantasmas_dev.travelapppet_project.ui.layouts
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    LazyColumn {
+        item { Text(text = "Aspen") }
+        item {
+            SearchBar(
+                query = "",
+                onQueryChange = {},
+                onSearch = {},
+                active = true,
+                onActiveChange = {}) {
 
+            }
+        }
+        item {
+            LazyRow {
+
+            }
+        }
+        item { Text(text = "") }
+        item {
+            LazyRow {
+                item {
+                    Card {
+
+                    }
+                }
+            }
+        }
+        item { Text(text = "") }
+        item {
+            LazyRow {
+                item {
+                    Card {
+
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 @Preview(
     showBackground = true,
