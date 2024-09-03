5759ab0798dacd3a5a7bf97a750a74e6afa01af5
FantasmasDe <fantasmas.dev@gmail.com>
2024-09-04 00:39:35 +0300

    правки

--- app/src/main/java/com/fantasmas_dev/travelapppet_project/ui/layouts/MainScreen.kt
+++ app/src/main/java/com/fantasmas_dev/travelapppet_project/ui/layouts/MainScreen.kt
@@ -2,22 +2,11 @@ package com.fantasmas_dev.travelapppet_project.ui.layouts
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarColors
-import androidx.compose.material3.SearchBarDefaults
-import androidx.compose.material3.SearchBarDefaults.inputFieldColors
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +17,6 @@ import androidx.compose.ui.unit.sp
 import com.fantasmas_dev.travelapppet_project.ui.layouts.widgets.AppSearchBar
 import com.fantasmas_dev.travelapppet_project.ui.theme.MontserratFont
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     Column(
@@ -61,7 +49,7 @@ fun MainScreen() {
             color = Color.Black
         )
 
-        AppSearchBar()
+        AppSearchBar(modifier = Modifier.padding(top = 24.dp))
     }
 
 
--- app/src/main/java/com/fantasmas_dev/travelapppet_project/ui/layouts/widgets/SearchBar.kt
+++ app/src/main/java/com/fantasmas_dev/travelapppet_project/ui/layouts/widgets/SearchBar.kt
@@ -2,7 +2,6 @@ package com.fantasmas_dev.travelapppet_project.ui.layouts.widgets
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
@@ -19,22 +18,22 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fantasmas_dev.travelapppet_project.R
 import com.fantasmas_dev.travelapppet_project.ui.theme.MontserratFont
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppSearchBar() {
+fun AppSearchBar(
+    modifier: Modifier = Modifier
+) {
     var searchText by remember {
         mutableStateOf("")
     }
 
     SearchBar(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp),
+        modifier = modifier
+            .fillMaxWidth(),
 
         query = searchText,
         onQueryChange = { text ->
@@ -55,7 +54,8 @@ fun AppSearchBar() {
             Image(
                 painter = painterResource(id = R.drawable.search_img),
                 contentDescription = "",
-                colorFilter = ColorFilter.tint(Color.DarkGray))
+                colorFilter = ColorFilter.tint(Color.DarkGray)
+            )
 
         },
         colors = SearchBarDefaults.colors(
